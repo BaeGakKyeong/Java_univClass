4588a2cbc7a8cd7f534a6867add441f902c6971b
BaeGakKyeong <bggcarl@gamil.com>
2026-03-10 15:17:19 +0900

    exercise01

---
+++ src/class02/Ex01.java
@@ -0,0 +1,13 @@
+package class02;
+
+public class Ex01 {
+    public static void main(String[] args){
+        var price = 200;
+        var name = "Kitae";
+        var pi = 3.14;
+
+        System.out.println("price = " + (price + 1000));
+        System.out.println("name = " + name);
+        System.out.println("pi = " + pi * 10);
+    }
+}
