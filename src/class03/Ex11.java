1a2c26d4e69bd5154a8cce661b4d311ee24494e9
BaeGakKyeong <bggcarl@gamil.com>
2026-03-24 13:40:46 +0900

    exception using throws

--- src/class03/Ex11.java
+++ src/class03/Ex11.java
@@ -2,8 +2,14 @@ package class03;
 import java.util.Scanner;
 
 public class Ex11 {
+
+    public int divide(int i, int j) throws ArithmeticException{
+        return i / j;
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        Ex11 ex = new Ex11();
+
         int x, y;
 
         System.out.println("나뉘는 수를 수를 입력하시오 : ");
@@ -13,11 +19,11 @@ public class Ex11 {
         y = scan.nextInt();
 
         try{
-            System.out.println(x + " / " + y + " = " + x / y);
+            System.out.println(x + " / " + y + " = " + ex.divide(x, y));
     
         }
         catch(ArithmeticException e){
-            System.out.println("0으로 나눌 수 없습니다.");
+            System.out.println(e.toString());
         }
         finally{
             scan.close();
