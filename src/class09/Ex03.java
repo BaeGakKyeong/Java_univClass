66f93139b3f1f82412197f3a85dc10c3259c3d9e
BaeGakKyeong <bggcarl@gamil.com>
2026-04-28 15:59:27 +0900

    swing GUI

---
+++ src/class09/Ex03.java
@@ -0,0 +1,26 @@
+package class09;
+import javax.swing.*;
+import java.awt.*;
+
+public class Ex03 extends JFrame{
+    public Ex03(){
+        setTitle("ContentPane과 JFrame예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Container contentPane = getContentPane();
+        contentPane.setBackground(Color.ORANGE);
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("OK"));
+        contentPane.add(new JButton("Cancel"));
+        contentPane.add(new JButton("Ignore"));
+
+        setSize(300, 150);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Ex03();
+    }
+}
+
